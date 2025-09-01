4769c81b98eff84af6f752012834771ed88d2c8f
gatsby6060 <uniline123@naver.com>
2025-09-01 15:59:11 +0900

    커밋

--- app.js
+++ app.js
@@ -336,6 +336,31 @@ app.get('/login', async (req, res) => {
 
 
 
+app.get('/user/info', async (req, res) => {
+  const { userId } = req.query; //보낼때의 이름 맞춰서... 받을것은 userId임
+  console.log(userId);
+
+  let query = `SELECT * FROM TBL_USER WHERE USERID= '${userId}' `
+  try {
+    const result = await connection.execute(query);
+    const columnNames = result.metaData.map(column => column.name);
+
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    res.json(rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
 
 
 
